8f845ffefdd586c1351938c01701c48b96e0d7e2
Mounia20 <maymunabdifatah@gmail.com>
2022-07-05 11:44:59 -0700

    c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,14 @@
 #include "main.h"
-#include <time.h>
-#include <stdio.h>
 int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar("\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putcha('\n');
 	return (0);
 }
