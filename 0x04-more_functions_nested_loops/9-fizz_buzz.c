4a03d4c214d4d91d7e4af7dd4854e4516e8e7006
Mounia20 <maymunabdifatah@gmail.com>
2022-07-07 20:59:06 -0700

    c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,20 @@
+#include "main.h"
+void fizz_buzz(void)
+{
+	int x;
+
+	for (x = 1; x < 100; x++)
+	{
+		if (x == 100)
+			printf("Buzz");
+		if (x % 3 == 0 && x % 5 == 0)
+			printf("FizzBuz");
+		else if (x % 3  == 0)
+			printf("Fizz");
+		else if (x % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", x);
+	}
+	printf("%d", x);
+}
