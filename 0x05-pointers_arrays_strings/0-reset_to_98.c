bc9a79a3184a130580c440c9f86f1ff34bf2d69a
Mounia20 <maymunabdifatah@gmail.com>
2022-07-12 14:23:45 -0700

    c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,5 @@
 #include "main.h"
-char *strcat(char *dest, char *src)
+void reset_to_98(int *n)
 {
-	int len =  0, i;
-
-	while (dest[len])
-	len++;
-	for (i = 0; src[i] != 0; i++)
-	{
-dest[len] = src[i];
-len += 1;
-	}
-	dest[len] = '\0';
-	return (dest);
+	*n = 98;
 }
