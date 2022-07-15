7289ae220c9d5ebaa4899040a95df617b7007ede
Mounia20 <maymunabdifatah@gmail.com>
2022-07-15 15:07:54 -0700

    c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+char *string_toupper(char *)
+{
+int i = 0;
+
+while (x[i])
+{
+	if (x[i] >= 97 && x[i] <= 122)
+		x[i] = x[i] - 32;
+i++;
+}
+return (x);
+}
